dfe39030c99314f32bb9a53fa15c25ea7c769d98
Brett Mandler <brettmandler@gmail.com>
2021-12-20 14:14:41 -0500

    Redirect to faq

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,25 @@ import Head from 'next/head'
 import {signIn} from 'next-auth/react'
 import {Button} from '../components/Button'
 import {NextPage} from 'next'
+import {useRouter} from 'next/dist/client/router'
+import {useEffect} from 'react'
 
-const Home: NextPage = () => (
-  <div>
-    <Head>
-      <title>Awesome Links</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Button className="btn-blue" onClick={() => signIn()}>
-      Sign In
-    </Button>
-  </div>
-)
+const Home: NextPage = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/faq')
+  })
+  return (
+    <div>
+      <Head>
+        <title>Awesome Links</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Button className="btn-blue" onClick={() => signIn()}>
+        Sign In
+      </Button>
+    </div>
+  )
+}
 
 export default Home
