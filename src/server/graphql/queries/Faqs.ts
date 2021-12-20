2039ade2bb9086bc86e25b012907cf2d90b18d6f
Brett Mandler <brettmandler@gmail.com>
2021-12-19 23:13:12 -0500

    Order faqs by id to maintain order

--- src/server/graphql/queries/Faqs.ts
+++ src/server/graphql/queries/Faqs.ts
@@ -2,5 +2,6 @@ import {list, queryField} from 'nexus'
 
 export const Faqs = queryField('faqs', {
   type: list('Faq'),
-  resolve: async (_root, _args, ctx) => ctx.prisma.faq.findMany(),
+  resolve: async (_root, _args, ctx) =>
+    ctx.prisma.faq.findMany({orderBy: {id: 'desc'}}),
 })
