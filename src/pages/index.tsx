7c494bc8cf8e0e806bf1ad256d79d0aa77c6f814
Brett Mandler <brettmandler@gmail.com>
2021-12-21 17:35:52 -0500

    Updated title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,25 +5,23 @@ import {NextPage} from 'next'
 import {getSession, GetSessionParams} from 'next-auth/react'
 import {Auth0} from '@styled-icons/simple-icons/Auth0'
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Awesome Links</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="container flex items-center justify-center max-w-full">
-        <Button
-          className="mx-auto mt-[30vh] btn-blue"
-          leftIcon={<Auth0 />}
-          onClick={() => signIn('auth0', {callbackUrl: '/faq'})}
-        >
-          Sign In With Auth0
-        </Button>
-      </div>
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>Spatulafoods Admin</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="container flex items-center justify-center max-w-full">
+      <Button
+        className="mx-auto mt-[30vh] btn-blue"
+        leftIcon={<Auth0 />}
+        onClick={() => signIn('auth0', {callbackUrl: '/faq'})}
+      >
+        Sign In With Auth0
+      </Button>
     </div>
-  )
-}
+  </div>
+)
 
 export async function getServerSideProps(context: GetSessionParams) {
   const session = await getSession(context)
