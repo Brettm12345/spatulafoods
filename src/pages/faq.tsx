bef23e4bc5ca0c8630a5402570dec774bf3b056d
Brett Mandler <brettmandler@gmail.com>
2021-12-21 10:55:07 -0500

    Added loading skeletons

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,26 +1,39 @@
 import {PlusIcon} from '@heroicons/react/outline'
 import {NextPage} from 'next'
 import {useState} from 'react'
+import {Spinner3} from 'styled-icons/icomoon'
 import {Button} from '../components/Button'
 import {Faq} from '../components/Faq'
 import {FaqModal} from '../components/FaqModal'
+import {Spinner} from '../components/Spinner'
 import {useFaqsQuery} from '../generated/graphql'
 
 const FaqPage: NextPage = () => {
-  const [{data}] = useFaqsQuery()
+  const [{data, fetching}] = useFaqsQuery()
   const [isOpen, setIsOpen] = useState(false)
   return (
     <div className="container flex flex-col max-w-lg pb-20 mx-auto mt-10 space-y-2">
-      {data?.faqs?.map(faq => (
-        <Faq key={faq.id} {...faq} />
-      ))}
-      <Button
-        className="mt-5 btn-blue max-w-fit"
-        onClick={() => setIsOpen(true)}
-      >
-        <PlusIcon /> Create FAQ
-      </Button>
-      <FaqModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      {fetching ? (
+        [...Array(25)].map((_, index) => (
+          <div
+            key={index}
+            className="w-full h-[58px] rounded-md animate-pulse bg-gray-100 dark:bg-gray-600"
+          />
+        ))
+      ) : (
+        <>
+          {data?.faqs?.map(faq => (
+            <Faq key={faq.id} {...faq} />
+          ))}
+          <Button
+            className="mt-5 btn-blue max-w-fit"
+            onClick={() => setIsOpen(true)}
+          >
+            <PlusIcon /> Create FAQ
+          </Button>
+          <FaqModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+        </>
+      )}
     </div>
   )
 }
