70055a602c4f77679e9b80e43f4ad5505a19e333
Brett Mandler <brettmandler@gmail.com>
2021-12-21 16:25:12 -0500

    Set softer bg color

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
   const session = useSession()
   console.log(session)
   return (
-    <header className="bg-white max-h-18 dark:bg-gray-900 px-2 sm:px-4 py-2.5">
+    <header className="bg-gray-50 max-h-18 dark:bg-gray-900 px-2 sm:px-4 py-2.5">
       <div className="flex flex-wrap items-center justify-between mx-auto max-h-14">
         <Link href="/">
           <Image
