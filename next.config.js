c12048141930062b7f1d959383a1308f827fa8fe
Brett Mandler <brettmandler@gmail.com>
2021-12-24 01:26:54 -0500

    Set locale

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 require('dotenv').config()
+
 module.exports = {
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
